d81df8a82ecd22a938c87f435b8db1a060962246
KenethSandoval <kenethra.74@gmail.com>
2021-09-20 21:45:41 -0600

    fix: Camera2D init

--- camera/camera.go
+++ camera/camera.go
@@ -1,13 +1,28 @@
 package camera
 
 import (
-	"fmt"
-
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
-func CameraInit() {
-  //player := rl.Rectangle{ 400, 280, 40, 40 }
-  buildings := [MAX_BUILDINGS]rl.RectangleInt32 { []int32{0} }
-  fmt.Println(player)
+func CameraInit(r *RayRectangle) {
+  player := rl.Rectangle{ 400, 280, 40, 40 }
+  var buildings [MAX_BUILDINGS]rl.Rectangle
+
+  var buildColor [MAX_BUILDINGS]rl.Color 
+  spacing := 0
+
+  for i := 0; i < MAX_BUILDINGS; i++ {
+    buildings[i].Width = float32(rl.GetRandomValue(50,200))
+    buildings[i].Height = float32(rl.GetRandomValue(100, 800))
+    buildings[i].Y = float32(r.ScreenHeight) - 130.0 - buildings[i].Height
+    buildings[i].X = float32(-6000.0) + float32(spacing)
+ 
+    spacing += int(buildings[i].Width)
+    
+    buildColor[i] = rl.Color{ uint8(rl.GetRandomValue(200,240)), uint8(rl.GetRandomValue(200,240)), uint8(rl.GetRandomValue(200,250)), 255 }
+  }
+
+  camera := rl.Camera2D{ 0 }
+  
+  camera.Target = rl.Vector2({ player.X + 20.0 player.Y + 20.0 })
 }
